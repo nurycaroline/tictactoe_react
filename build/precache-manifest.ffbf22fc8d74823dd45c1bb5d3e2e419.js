114a8b6d362b1ce831645dd8d61be15bc861cc11
nurycaroline <nurycaroline@gmail.com>
2019-10-28 15:21:46 -0300

    build

---
+++ build/precache-manifest.ffbf22fc8d74823dd45c1bb5d3e2e419.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00c73ccfdc11f5ca3a7fe20579cd88f2",
+    "url": "/tictactoe_react/index.html"
+  },
+  {
+    "revision": "620c08fd9479c6303f6a",
+    "url": "/tictactoe_react/static/css/main.90255786.chunk.css"
+  },
+  {
+    "revision": "20fba7c11165b549f4de",
+    "url": "/tictactoe_react/static/js/2.462176f7.chunk.js"
+  },
+  {
+    "revision": "620c08fd9479c6303f6a",
+    "url": "/tictactoe_react/static/js/main.e431fc26.chunk.js"
+  },
+  {
+    "revision": "646c975706bd6c850edb",
+    "url": "/tictactoe_react/static/js/runtime-main.edc62d51.js"
+  },
+  {
+    "revision": "c1bf471f65ee2ff923c5d1a8a89ed9c0",
+    "url": "/tictactoe_react/static/media/188169.c1bf471f.svg"
+  }
+]);
